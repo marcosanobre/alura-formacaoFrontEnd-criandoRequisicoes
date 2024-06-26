ef04e882e817a2e762047827c24ee0a0376cdd53
Marcos Nobre <marconobre@gmail.com>
2024-06-25 23:49:40 -0300

    Implementando POST

---
+++ js/conectaApi.js
@@ -0,0 +1,36 @@
+async function listaVideos() {
+    const conexao = await fetch("http://localhost:3000/videos");
+    const conexaoJson = await conexao.json();
+    return conexaoJson;
+};
+
+async function criaVideo(titulo,descricao,url,imagem) {
+    const conexao = await fetch( "http://localhost:3000/videos", {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify( {
+            titulo: titulo,
+            descricao: `${descricao} mil visualizações`,
+            url: url,
+            imagem: imagem
+        })
+    });
+
+    const conexaoConvertida = await conexao.json();
+
+    return conexaoConvertida;
+};
+
+export const conectaApi = {
+    listaVideos,
+    criaVideo
+};
+
+
+
+
+
+
+
