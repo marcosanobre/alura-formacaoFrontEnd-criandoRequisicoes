044342a9d34febc9795c30b83e5c1e829e194547
Marcos Nobre <marconobre@gmail.com>
2024-07-01 22:20:22 -0300

    Finalizado os exercicios

--- js/buscarVideo.js
+++ js/buscarVideo.js
@@ -3,9 +3,7 @@ import constroiCard from "./mostrarVideos.js";
 
 async function buscarVideo( evento ) {
     evento.preventDefault();
-
     const dadosDePesquisa = document.querySelector('[data-pesquisa]').value;
-
     const busca = await conectaApi.buscaVideo( dadosDePesquisa );
 
     const lista = document.querySelector("[data-lista]");
@@ -14,17 +12,21 @@ async function buscarVideo( evento ) {
         lista.removeChild( lista.firstChild );
     };
 
-    busca.forEach( elemento => 
-        lista.appendChild( constroiCard( elemento.titulo,
-                                         elemento.descricao,
-                                         elemento.url,
-                                         elemento.imagem
-                                        )
-                         )
-                 );
+    if (busca.length ==0) {
+        lista.innerHTML = `<h2 class="mensagem__titulo__erro">Não existem vídeos, para este termo de Pesquisa.</h2>`;
+    } else {
+        busca.forEach( elemento => 
+            lista.appendChild( constroiCard( elemento.titulo,
+                                             elemento.descricao,
+                                             elemento.url,
+                                             elemento.imagem
+                                            )
+                             )
+                     );    
+    };
 };
 
 const botaoDePesquisa = document.querySelector("[data-botao-pesquisa]");
 
-botaoDePesquisa.addEventListener( 'click', evento => buscarVideo( evento) );
+botaoDePesquisa.addEventListener( "click", evento => buscarVideo( evento) );
 
--- js/conectaApi.js
+++ js/conectaApi.js
@@ -17,12 +17,17 @@ async function criaVideo(titulo,descricao,url,imagem) {
             imagem: imagem
         })
     });
+    if ( ! conexao.ok ) {
+        throw new Error("Não foi possível enviar o vídeo !!!");
+    };
     const conexaoConvertida = conexao.json();
     return conexaoConvertida;
 };
 
 async function buscaVideo( termoDeBusca ) {
-    const conexao = await fetch( `http://localhost:3000/videos?q=${termoDeBusca}` );
+    const fetchURI = `http://localhost:3000/videos?q="${termoDeBusca}"`;
+console.log(fetchURI);
+    const conexao = await fetch( fetchURI );
     const conexaoJSON = await conexao.json();
     return conexaoJSON;
 };
