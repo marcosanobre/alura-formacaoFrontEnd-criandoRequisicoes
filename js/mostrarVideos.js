21d227fa70f68a5a3f7c953f09981389e195da78
Marcos Nobre <marconobre@gmail.com>
2024-07-24 16:32:25 -0300

    Atualização

--- js/mostrarVideos.js
+++ js/mostrarVideos.js
@@ -2,7 +2,7 @@ import { conectaApi } from "./conectaApi.js";
 
 const lista = document.querySelector("[data-lista]");
 
-export default function constroiCard( titulo, descricao, url, imagem ) {
+export default function constroiCard( indice, titulo, descricao, url, imagem ) {
     const video = document.createElement('li');
     video.className = 'videos__item';
     video.innerHTML = `
@@ -10,6 +10,10 @@ export default function constroiCard( titulo, descricao, url, imagem ) {
             title="${titulo}" frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowfullscreen></iframe>
+        <div class="video-acoes__container">
+        <a class="link-acoes" href="http://127.0.0.1:5500/pages/enviar-video.html?exc=${indice}" id="a-link-excluivideo">Exclui</a>
+        <a class="link-acoes" href="http://127.0.0.1:5500/pages/enviar-video.html?alt=${indice}" id="a-link-alteravideo">Altera</a>
+        </div>
         <div class="descricao-video">
             <img src="${imagem}" alt="logo canal alura">
             <h3>${titulo}</h3>
@@ -22,7 +26,7 @@ export default function constroiCard( titulo, descricao, url, imagem ) {
 async function listaVideos() {
     try {
         const listaApi = await conectaApi.listaVideos();
-        listaApi.forEach( elemento => lista.appendChild( constroiCard( 
+        listaApi.forEach( (elemento, index) => lista.appendChild( constroiCard( index, 
                                                                         elemento.titulo,
                                                                         elemento.descricao,
                                                                         elemento.url,
